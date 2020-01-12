b4a679af451a01a09f71d750f04c11ac3bececc2
lasz <57516687+lasz-buis@users.noreply.github.com>
2020-01-12 17:34:35 +0200

    starting course 4 project

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -2,7 +2,7 @@ const apiId = '00kau6hk72'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'test-endpoint.auth0.com',
-  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  domain: 'dev-faw3vy6e.auth0.com',
+  clientId: 'lSRaNNE8EMnwBINCaXOLM84YLlXucveh',
   callbackUrl: 'http://localhost:3000/callback'
 }

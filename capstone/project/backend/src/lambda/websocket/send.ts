5ee1141902006409f2b96cb5f651747b1d791d4b
lasz <57516687+lasz-buis@users.noreply.github.com>
2020-02-01 14:03:55 +0200

    broke up monolith send function

--- capstone/project/backend/src/lambda/websocket/send.ts
+++ capstone/project/backend/src/lambda/websocket/send.ts
@@ -1,100 +1,145 @@
 import 'source-map-support/register'
 import { socket_disconnect } from '../../businessLogic/websocket'
 import { socket_scan } from '../../businessLogic/websocket'
+import * as AWS	from 'aws-sdk'
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { BroadcastPayload } from '../../models/broadcast_payload'
 
-//
-import * as AWS  from 'aws-sdk'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-const docClient: DocumentClient = new AWS.DynamoDB.DocumentClient();
-import { UnicastPayload } from '../../models/unicast_payload'
-const domain = 'wss://3t5d4gcuz4.execute-api.us-west-2.amazonaws.com';
-const stage = 'dev'
-const connectionParams = 
-{
-    apiVersion: "2018-11-29",
-    endpoint: `${domain}/${stage}`
-}
-const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams);
-const connectionsTable = process.env.CONNECTIONS_TABLE;
-//
-
+import {ChatAccess} from '../../dataLayer/chatAccess'
+const chatAccess = new ChatAccess();
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => 
 {
-	console.log("Endpoint :" + `${domain}/${stage}`);
+	const endpoint : string = event.requestContext.domainName + "/" + event.requestContext.stage;
 	const senderId : string = event.requestContext.connectionId;
-	
-	const message = JSON.parse(event.body).data;
-	await broadcast ({senderId , message});
+	const payload : string = JSON.stringify(JSON.parse(event.body).data);
+	const sendPacket : BroadcastPacket =
+	{
+		endpoint,
+		senderId,
+		payload,
+	}
+	await broadcast (sendPacket);
 	return {
 		statusCode: 200,
 		body: ''
-  	}
+	}
 }
-
-async function broadcast (broadcast_payload : BroadcastPayload)
+//******************************************************************************
+async function broadcast (packet : BroadcastPacket)
 {
-  console.log ("Scanning all connections");
-  const connections = await socket_scan();
-  console.log("SCAN RESULTS: " + JSON.stringify (connections))
-  console.log ("Broadcasting");
-  const senderId : string = broadcast_payload.senderId;
-  const message : string = broadcast_payload.message
-  for (const connection of connections.Items) 
-  {
-    const receiverId : string = connection.id;
-    const unicast_payload : UnicastPayload = 
-    {
-      senderId,
-      receiverId,
-      message
-    }
-    await send (unicast_payload);
-  }
-}
-
-async function send (payload : UnicastPayload)
-{
-  try
-  {
-    console.log ("SEND Payload: " + JSON.stringify(payload));
-    const senderAlias : string = await getAlias(payload.senderId);
-    const receiverAlias : string = await getAlias(payload.receiverId); 
-    if (senderAlias && receiverAlias)
-    {
-      console.log(senderAlias + '=>'+ receiverAlias);
-    }
-    await apiGateway.postToConnection(
-    {
-      ConnectionId: payload.receiverId,
-      Data: senderAlias + ' : ' + payload.message,
-    }).promise();
-  } 
-  catch (e)
-  {
-    console.log('Failed to send message', JSON.stringify(e))
-    if (e.statusCode === 410) 
-    {
-      console.log('Stale connection')
-      await socket_disconnect (payload.receiverId);
-    }
-  }
-}
-
-async function getAlias (connectionId : string) : Promise <string>
-{
-	const result = await docClient.query(
+	const connections = await socket_scan();	
+	for (const connection of connections.Items) 
 	{
-    TableName: connectionsTable,
-    KeyConditionExpression: 'id = :id',
-    ExpressionAttributeValues: 
-    {
-      ':id': connectionId
-    }
-  }).promise();
-	const alias : string = result.Items[0].alias;
-	console.log ('Alias Returned from database :' + alias);
-	return alias;
+		const receiverId : string = connection.id;
+		try
+		{
+			const endpoint = packet.endpoint;
+			const senderId = packet.senderId
+			const payload = packet.payload
+			const sendPacket : UnicastPacket =
+			{
+				endpoint,
+				senderId,
+				receiverId,
+				payload
+			}
+			await send (sendPacket);
+		} 
+		catch (e)
+		{
+			console.log('Failed to send message', JSON.stringify(e))
+			if (e.statusCode === 410) 
+			{
+				console.log('Stale connection')
+				await socket_disconnect (receiverId);
+			}
+		}
+	}
+}
+
+// async function multicast (packet : MulticastPacket)
+// {
+// 	const topic = packet.topic;
+// 	const connections = await socket_query (topic);	
+// 	for (const connection of connections.Items) 
+// 	{
+// 		const receiverId : string = connection.id;
+// 		try
+// 		{
+// 			const endpoint = packet.endpoint;
+// 			const senderId = packet.senderId
+// 			const payload = packet.payload
+// 			const sendPacket : UnicastPacket =
+// 			{
+// 				endpoint,
+// 				senderId,
+// 				receiverId,
+// 				payload
+// 			}
+// 			await send (sendPacket);
+// 		} 
+// 		catch (e)
+// 		{
+// 			console.log('Failed to send message', JSON.stringify(e))
+// 			if (e.statusCode === 410) 
+// 			{
+// 				console.log('Stale connection')
+// 				await socket_disconnect (connectionId);
+// 			}
+// 		}
+// 	}
+// }
+
+async function send (packet : UnicastPacket)
+{
+	const endpoint : string = packet.endpoint;
+	const receiverId : string = packet.receiverId;
+	const senderId : string = packet.senderId;
+	const payload : string = packet.payload;
+	console.log('Sending message to a connection', receiverId);
+
+	const senderAlias : string = await chatAccess.queryAlias (senderId);
+	const receiverAlias : string = await chatAccess.queryAlias (receiverId);
+	console.log (senderAlias + ' => ' + receiverAlias + ' : ' + payload);
+	
+	const taggedPayload : string = senderAlias + ' : ' + payload;
+	await DL_SEND (endpoint, receiverId, taggedPayload);
+}
+//******************************************************************************
+async function DL_SEND (endpoint, receiverId, payload)
+{
+	const connectionParams = 
+	{
+		apiVersion: "2018-11-29",
+		endpoint: endpoint
+	}
+	const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams);
+	await apiGateway.postToConnection(
+	{
+		ConnectionId: receiverId,
+		Data: payload,
+	}).promise();
+}
+//******************************************************************************
+interface BroadcastPacket
+{
+	endpoint : string
+	senderId : string
+	payload : string
+}
+
+// interface MulticastPacket
+// {
+// 	endpoint : string
+// 	topic : string
+// 	senderId : string
+// 	payload : string
+// }
+
+interface UnicastPacket
+{
+	endpoint : string
+	senderId : string
+	receiverId: string
+	payload : string
 }
